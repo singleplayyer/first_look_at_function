1e69fcf52f3cb4da729d48e9ca8db2a6c92a2def
singleplayyer <31764917+singleplayyer@users.noreply.github.com>
2017-10-30 01:05:20 +0300

    Add files via upload

---
+++ some_first_testing.js
@@ -0,0 +1,36 @@
+// SOME FIRS TESTS OF FUNC
+
+function one (i) {
+	if (i === 1) {
+		return "Hello world";
+	} else if (i === 0) {
+		return "Not hello me here";
+	} else { return i;}
+}
+console.log(one());
+
+***************************************
+
+function concat(i,j) {
+	return i + j;
+}
+console.log(concat("Hello ", "Vladi"));
+
+***************************************
+
+fun();
+function fun() {
+	console.log("You are awesome");
+}
+
+***************************************
+
+var b = 1;
+function scd(a = 1) {
+	return a + b;
+}
+function lst() {
+	return 1;
+}
+
+console.log(scd() + lst());
